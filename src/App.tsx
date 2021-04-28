435baac6780a9035c9401fd6a945396435420a77
neillycubed <neillybusiness3@gmail.com>
2021-04-27 23:55:32 -0700

    create App component and render into DOM

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    const env = process.env.NODE_ENV
+
+    return(
+        <div>
+            <h1 className="text-yellow-50">Hello world</h1>
+        </div>
+    )
+};
+
+export default App;
