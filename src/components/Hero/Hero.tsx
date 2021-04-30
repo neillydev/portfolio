044eb3ebbf041182f0555898d310d229eb424db8
neillycubed <neillybusiness3@gmail.com>
2021-04-29 22:20:51 -0700

    create Hero component for hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Hero = () => {
+    return (
+        <div className="bg-white h-screen flex flex-col 
+        justify-center items-center">
+            <h1 className="lg:text-9xl md:text-7xl sm:text-5xl 
+            text-3xl font-black mb-14">
+                Vernon Neilly
+            </h1>
+        </div>
+    )
+}
+
+export default Hero
