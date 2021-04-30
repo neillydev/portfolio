5fc966e84b0f64bf2e7a391aac2b12050fc647d8
neillycubed <neillybusiness3@gmail.com>
2021-04-30 11:57:09 -0700

    add full name to NavBar title

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () => {
         font-mono'
         role='navigation'>
             <Link to='/' className='pl-8'>
-                Vernon
+                Vernon Neilly
             </Link>
             <div className="px-4 cursor-pointer md:hidden">
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
