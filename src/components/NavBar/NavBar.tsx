a52e082777e402606661e15adc520c895be92fa5
neillycubed <neillybusiness3@gmail.com>
2021-05-01 01:46:28 -0700

    add styling to NavBar icons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -17,12 +17,14 @@ const NavBar = () => {
             <div className="px-4 cursor-pointer md:hidden">
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
             </div>
-            <div className="pr-8 xl:flex lg:flex md:flex sm:hidden hidden">
-                <div className="m-8">
+            <div className="pr-8 items-center xl:flex lg:flex md:flex sm:hidden hidden">
+                <div className="m-8 flex justify-content items-center">
                     <TwitterIcon />
+                    <p className="font-sans font-semibold text-sm ml-4">Twitter</p>
                 </div>
-                <div className="m-8">
+                <div className="m-8 flex justify-content items-center">
                     <GitIcon />
+                    <p className="font-sans font-semibold text-sm ml-4">Github</p>
                 </div>
             </div>
         </nav>
