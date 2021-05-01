7539ed5ef6232a384e87245efb691f38fabf1c83
neillycubed <neillybusiness3@gmail.com>
2021-05-01 01:38:29 -0700

    add twitter and git icons to NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import TwitterIcon from '../Icons/TwitterIcon';
+import GitIcon from '../Icons/GitIcon';
+
 const styles = require('./NavBar.css');
 
 const NavBar = () => {
@@ -9,16 +12,18 @@ const NavBar = () => {
         font-sans font-bold'
         role='navigation'>
             <Link to='/' className='pl-8'>
-                Vernon Neilly
+                &lt; Vernon Neilly /&gt;
             </Link>
             <div className="px-4 cursor-pointer md:hidden">
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
             </div>
-            <div className="pr-8 md:block hidden">
-                <Link className="p-4" to='/'>Home</Link>
-                <Link className="p-4" to='/menu'>Menu</Link>
-                <Link className="p-4" to='/about'>About</Link>
-                <Link className="p-4" to='/contact'>Contact</Link>
+            <div className="pr-8 xl:flex lg:flex md:flex sm:hidden hidden">
+                <div className="m-8">
+                    <TwitterIcon />
+                </div>
+                <div className="m-8">
+                    <GitIcon />
+                </div>
             </div>
         </nav>
     )
