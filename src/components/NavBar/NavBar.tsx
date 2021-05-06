64888a41b4e33a45e0fdde467718b446c01f07d8
neillycubed <neillybusiness3@gmail.com>
2021-05-05 23:47:04 -0700

    add new navigation buttons for NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -19,14 +19,30 @@ const NavBar = () => {
         <nav className='flex items-center h-16 bg-white text-black relative shadow-sm
         font-sans font-bold'
             role='navigation'>
-            <div className="flex items-center justify-between w-screen">
-                <Link to='/' className='pl-8'>
+            <div className="flex items-center w-screen">
+                <Link to='/' className='pl-32 text-lg'>
                     &lt; Vernon Neilly /&gt;
                 </Link>
                 <div className="px-4 cursor-pointer md:hidden">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </div>
-                <div className="pr-8 items-center xl:flex lg:flex md:flex sm:hidden hidden">
+            </div>
+            <div className="flex flex-end">
+                <div className='flex items-center mr-8'>
+                    <button className="navBtn items-center justify-content mr-8">
+                        <p className="font-sans font-bold text-md ml-4">Projects</p>
+                        <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
+                    </button>
+                    <button className="navBtn mr-8 w-24">
+                        <p className="font-sans font-bold text-md ml-4">About Me</p>
+                        <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
+                    </button>
+                    <button className="navBtn mr-8">
+                        <p className="font-sans font-bold text-md ml-4">Contact</p>
+                        <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
+                    </button>
+                </div>
+                <div className="pr-12 items-end xl:flex lg:flex md:flex sm:hidden hidden">
                     <button className="navBtn m-8 flex justify-content items-center" onClick={() => handleTwitterBtn()}>
                         <TwitterIcon />
                         <p className="font-sans font-semibold text-sm ml-4">Twitter</p>
@@ -42,5 +58,7 @@ const NavBar = () => {
         </nav>
     )
 }
+
+/*  */
 
 export default NavBar
