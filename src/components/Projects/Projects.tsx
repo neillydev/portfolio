08418ac546d84d3e8b7b494b4f9739c1a4d4acef
neillycubed <neillybusiness3@gmail.com>
2021-05-09 02:25:54 -0700

    create Projects section component

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className='flex mt-72'>
+            <div className="flex flex-col">
+                <h1 className='lg:text-6xl md:text-5xl sm:text-4xl 
+            text-3xl font-black ml-52'>Latest Projects</h1>
+            <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6 lg:ml-64 md:ml-16 sm:ml-16' />
+            <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6 lg:ml-72 md:ml-16 sm:ml-16' />
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
