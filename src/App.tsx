9d104008d213d07a61e1ed8daffe6518312b89bb
neillycubed <neillybusiness3@gmail.com>
2021-05-10 20:21:47 -0700

    add ref forwarding for scrolling animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
+
 import './styles.css';
+require('./App.css');
 
 const App = () => {
     const env = process.env.NODE_ENV
 
+    const projRef = useRef<HTMLDivElement>(null);
+    const handleProjectBtn = () => {
+        if(projRef.current){
+            projRef.current.scrollIntoView();
+        }
+    }
+
     return (
         <>
-            <NavBar />
+            <NavBar handleProjectBtn={handleProjectBtn} />
             <Hero />
-            <Projects />
+            <Projects refObj={projRef} />
         </>
     );
 };
