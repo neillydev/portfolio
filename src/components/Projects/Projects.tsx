d6411a190ac94d76f0284dbfd9436d52fd63c688
neillycubed <neillybusiness3@gmail.com>
2021-05-12 13:42:57 -0700

    fix styling issues with projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -6,15 +6,17 @@ type ProjectProps = {
     refObj: LegacyRef<HTMLDivElement> | undefined
 };
 
-const Projects = ({refObj}: ProjectProps) => {
+const Projects = ({ refObj }: ProjectProps) => {
 
     return (
-        <div ref={refObj} className='projContainer flex mt-72'>
+        <div ref={refObj} className='projContainer flex xl:pl-52'>
             <div className="flex flex-col">
                 <h1 className='lg:text-6xl md:text-5xl sm:text-4xl 
-            text-3xl font-black ml-52'>Latest Projects</h1>
-            <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6 lg:ml-64 md:ml-16 sm:ml-16' />
-            <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6 lg:ml-72 md:ml-16 sm:ml-16' />
+            text-3xl font-black'>Latest Projects</h1>
+                <div className="barContainer flex flex-col">
+                    <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6' />
+                    <span className='bg-green-400 lg:ml-24 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6' />
+                </div>
             </div>
         </div>
     )
