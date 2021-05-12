53ddfd6e6bcd441e874e8a190228afb9efb9e814
neillycubed <neillybusiness3@gmail.com>
2021-05-12 13:43:10 -0700

    fix styling issues with Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const styles = require('./Hero.css');
+require('./Hero.css');
 
 const Hero = () => {
     return (
-        <div className='bg-white flex flex-col sm:mt-52'>
-            <div className="flex flex-col lg:ml-52 lg:mb-4 md:ml-28 md:mb-2 sm:ml-28 sm:mb-2">
+        <div className='heroContainer bg-white flex flex-col xl:pl-52 xl:pt-52'>
+            <div className="flex flex-col md:mb-2 sm:mb-2">
                 <span className='shadow-lg bg-green-400 lg:w-52 md:w-44 sm:w-36 lg:h-8 md:h-6 sm:h-5 rounded-sm flex items-center justify-center'>
                     <h1 className='font-mono font-semibold lg:text-base md:text-sm sm:text-xs'>Full Stack Developer</h1>
                 </span>
             </div>
             <h1 className='heroTitle lg:text-9xl md:text-7xl sm:text-5xl 
-            text-3xl font-black ml-52'>
+            text-3xl font-black'>
                 VERNON
                 <br />
                 NEILLY
             </h1>
-            <div className='flex flex-col lg:ml-60 lg:mt-10 md:mt-6 sm:mt-6 md:ml-32 sm:ml-28'>
+            <div className='flex flex-col lg:mt-10 md:mt-6 sm:mt-6'>
                 <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md' />
                 <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-10 md:mt-6 sm:mt-6 lg:ml-24 md:ml-16 sm:ml-16' />
             </div>
