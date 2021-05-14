5e2b73d94c9418afd4f4ecbf67b09c08ca86053c
neillycubed <neillybusiness3@gmail.com>
2021-05-13 21:55:49 -0700

    create Card component for projects

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+require('./Card.css');
+
+const Card = () => {
+    return (
+        <div className="cardContainer">
+
+        </div>
+    )
+}
+
+export default Card
