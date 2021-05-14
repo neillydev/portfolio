c7a1714be5e62c780ecd15a78b5171465080b3b3
neillycubed <neillybusiness3@gmail.com>
2021-05-13 21:56:06 -0700

    add Featured component to Projects section

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,4 +1,5 @@
 import React, { LegacyRef } from 'react';
+import Featured from '../Featured/Featured';
 
 const styles = require('./Projects.css');
 
@@ -9,14 +10,17 @@ type ProjectProps = {
 const Projects = ({ refObj }: ProjectProps) => {
 
     return (
-        <div ref={refObj} className='projContainer flex xl:pl-72 xl:pt-52'>
-            <div className="flex flex-col">
+        <div ref={refObj} className='projContainer flex flex-col'>
+            <div className="flex flex-col xl:pl-72 xl:pt-32">
                 <h1 className='lg:text-6xl md:text-5xl sm:text-4xl 
-            text-3xl font-black'>Latest Projects</h1>
+            text-3xl font-black'>Featured Projects</h1>
                 <div className="barContainer flex flex-col">
                     <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6' />
                     <span className='bg-green-400 lg:ml-24 lg:w-72 md:w-48 sm:w-28 lg:h-1.5 md:h-1 sm:h-1 rounded-md lg:mt-8 md:mt-6 sm:mt-6' />
                 </div>
+            </div>
+            <div className="featuresContainer xl:pl-80 xl:pt-20">
+                <Featured />
             </div>
         </div>
     )
