bd5b24b0979a328f3ba0ea1386f95bdecd326c98
neillycubed <neillybusiness3@gmail.com>
2021-05-13 21:55:34 -0700

    create Featured component for featured projects

---
+++ src/components/Featured/Featured.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+require('./Featured.css');
+
+const Featured = () => {
+    return (
+        <div className="featuredContainer">
+            <div className="imageContainer flex border-2 border-green-400 rounded-md"></div>
+        </div>
+    )
+}
+
+export default Featured
