968ac3304d140f91d3cd3b3c55b79d0efb8d01f8
neillycubed <neillybusiness3@gmail.com>
2021-05-18 20:43:09 -0700

    add footer elements and composition

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Repo from '../Icons/Repo';
 
 require('./Footer.css');
 
 const Footer = () => {
     return (
-        <footer className="footerContainer flex w-full h-8 justify-center items-center">
-            <div className="footerCredit font-mono">
-                Built by Vernon Neilly
+        <footer className="footerContainer flex w-full justify-center items-center">
+            <div className="footerDiv flex justify-center items-center rounded-lg bg-green-400 h-16 w-72">
+                <a href='https://github.com/neillydev' rel='noopener noreferrer' target='_blank'>
+                    <div className="footerCredit font-mono text-gray-100 text-sm">
+                        Built by Vernon Neilly III
+                    </div>
+                    <div className="">
+                        <span className=''>
+
+                        </span>
+                        <span>
+
+                        </span>
+                    </div>
+                </a>
             </div>
         </footer>
     )
