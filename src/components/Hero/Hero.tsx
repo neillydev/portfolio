fe22e8762dc87a6af2e7e9f3081a1c856eaae0f9
neillycubed <neillybusiness3@gmail.com>
2021-05-24 17:06:14 -0700

    add mock code and styling to Hero code window

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -35,15 +35,84 @@ const Hero = ({ handleProjectBtn }: HeroProps) => {
                 </button>
                 </div>
             </div>
-            <div className="codeContainer flex rounded absolute shadow-xl border-2">
-                <div className="mockControls flex">
-                    <span className="mockWindowControl bg-red-500 rounded-xl" />
-                    <span className="mockWindowControl bg-yellow-500 rounded-xl" />
-                    <span className="mockWindowControl bg-green-500 rounded-xl" />
+            <div className="codeContainer flex flex-col rounded absolute shadow-xl border-2">
+                <div className="codeHeader flex">
+                    <div className="mockControls flex">
+                        <span className="mockWindowControl bg-red-500 rounded-xl" />
+                        <span className="mockWindowControl bg-yellow-500 rounded-xl" />
+                        <span className="mockWindowControl bg-green-500 rounded-xl" />
+                        <div className="mockWindowName flex w-16 h-4">
+                            <p className="mockCodeFile text-xs text-gray-500">~/src/index.ts</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="codeBody flex ml-16">
+                    <ol className="code">
+                        <li>
+                            <span className="codeLine">
+                                <span className="var">
+                                    interface 
+                                </span>
+                                &nbsp;Person = {`{`} 
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;name: string;
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;occupation: string;
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;yearsOfExperience: number;
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                                {`}`};
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                                <span className="var">
+                                    var 
+                                </span>
+                                &nbsp;Vernon = {`{`} 
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;name: "Vernon",
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;occupation: "Full Stack Developer",
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                            &nbsp;&nbsp;&nbsp;&nbsp;yearsOfExperience: 10
+                            </span>
+                        </li>
+                        <li>
+                            <span className="codeLine">
+                                {`}`};
+                            </span>
+                        </li>
+                    </ol>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Hero
+export default Hero;
