b9aeaab6231ef38969e9199168f3151f5961850b
neillycubed <neillybusiness3@gmail.com>
2021-05-26 14:22:06 -0700

    add props to Contact for passing ref object

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { LegacyRef } from 'react';
 
 require('./Contact.css');
 
-const Contact = () => {
+type ContactProps = {
+    refObj: LegacyRef<HTMLDivElement> | undefined
+}
+
+const Contact = ({ refObj }: ContactProps) => {
     return (
-        <div className="contactContainer flex flex-col mb-20 justify-center items-center">
+        <div ref={refObj} className="contactContainer flex flex-col mb-20 justify-center items-center">
             <div className="contactHeader flex justify-center">
                 <h1 className="font-black text-6xl">Let's Chat</h1>
             </div>
             <div className="contactBody flex justify-center m-16">
-                <h1 className="font-sans font-normal text-xl">Currently looking for new opportunities! Shoot me a message, and let's get the ball rolling and talk about your project!</h1>
+                <h1 className="font-sans font-semibold text-xl">Currently looking for new opportunities! Shoot me a message, and let's get the ball rolling and talk about your project!</h1>
             </div>
-            <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-36">
+            <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-36" onClick={() => null}>
                 Contact Me
             </button>
         </div>
