bceeb17afcc9e3f71dcd0d7fbb7ec06a84fc5fbf
neillycubed <neillybusiness3@gmail.com>
2021-05-26 13:56:45 -0700

    add text to Contact section body

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -8,8 +8,8 @@ const Contact = () => {
             <div className="contactHeader flex justify-center">
                 <h1 className="font-black text-6xl">Let's Chat</h1>
             </div>
-            <div className="contactBody flex justify-center m-8">
-                <h1 className="font-sans font-medium text-xl">Test</h1>
+            <div className="contactBody flex justify-center m-24">
+                <h1 className="font-sans font-normal text-xl">Currently looking for new opportunities! Shoot me a message, and let's get the ball rolling!</h1>
             </div>
             <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-44">
                 Contact Me
