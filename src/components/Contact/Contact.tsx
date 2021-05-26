8a6af3225402536bc36cabab5c0e77453d822010
neillycubed <neillybusiness3@gmail.com>
2021-05-26 13:49:59 -0700

    add Contact button and header to section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+require('./Contact.css');
 
 const Contact = () => {
     return (
-        <div className="contactContainer">
-            
+        <div className="contactContainer flex flex-col mb-20 justify-center items-center">
+            <div className="contactHeader flex justify-center">
+                <h1 className="font-black text-6xl">Let's Chat</h1>
+            </div>
+            <div className="contactBody flex justify-center m-8">
+                <h1 className="font-sans font-medium text-xl">Test</h1>
+            </div>
+            <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-44">
+                Contact Me
+            </button>
         </div>
     )
 }
