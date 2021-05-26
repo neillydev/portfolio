2cb5d51a13061e4cd48621e70d5b722947695424
neillycubed <neillybusiness3@gmail.com>
2021-05-26 14:15:04 -0700

    add styling and content changes to Contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -8,10 +8,10 @@ const Contact = () => {
             <div className="contactHeader flex justify-center">
                 <h1 className="font-black text-6xl">Let's Chat</h1>
             </div>
-            <div className="contactBody flex justify-center m-24">
-                <h1 className="font-sans font-normal text-xl">Currently looking for new opportunities! Shoot me a message, and let's get the ball rolling!</h1>
+            <div className="contactBody flex justify-center m-16">
+                <h1 className="font-sans font-normal text-xl">Currently looking for new opportunities! Shoot me a message, and let's get the ball rolling and talk about your project!</h1>
             </div>
-            <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-44">
+            <button className="contactBtn border-2 border-green-400 text-green-500 rounded font-bold w-36">
                 Contact Me
             </button>
         </div>
