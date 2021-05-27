0a15e12f98215ff3a2367ad076c53991a7120f74
neillycubed <neillybusiness3@gmail.com>
2021-05-27 02:01:15 -0700

    add Modal header and body divs

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -15,7 +15,19 @@ const Modal = ({ handleModalActive }: ModalProps) => {
             </div>
             <div className="modalLayer flex items-center justify-center" onClick={() => handleModalActive(false)}>
                 <form>
-                    <div className="modalBody rounded-lg bg-green-400"></div>
+                    <div className="modalLayerContainer rounded-lg bg-green-400">
+                        <div className="modalHeader flex justify-center">
+                            <h1 className="text-white font-black font-sans p-8 text-3xl">Contact Me</h1>
+                        </div>
+                        <div className="modalBody">
+                            <div className="modalSubjectHeader">
+
+                            </div>
+                            <div className="modalBodyHeader">
+                                
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
