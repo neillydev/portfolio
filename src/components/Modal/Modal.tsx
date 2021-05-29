cb8dda39d8feb73fa5befd0875bc949095096824
neillycubed <neillybusiness3@gmail.com>
2021-05-28 18:56:03 -0700

    add subject and body elements to contact modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,14 +17,24 @@ const Modal = ({ handleModalActive }: ModalProps) => {
                 <form>
                     <div className="modalLayerContainer rounded-lg bg-green-400">
                         <div className="modalHeader flex justify-center">
-                            <h1 className="text-white font-black font-sans p-8 text-3xl">Contact Me</h1>
+                            <h1 className="text-white font-black font-sans p-4 text-3xl">Contact Me</h1>
                         </div>
                         <div className="modalBody">
-                            <div className="modalSubjectHeader">
-
+                            <div className="modalSubjectHeader p-4">
+                                <h5 className="subjectHeader text-white text-lg font-bold">
+                                    SUBJECT
+                                </h5>
+                                <div className="inputContainer">
+                                    <input type="text" className="modalInputSubject rounded" />
+                                </div>
                             </div>
-                            <div className="modalBodyHeader">
-                                
+                            <div className="modalBodyHeader p-4">
+                                <h5 className="bodyHeader text-white text-lg font-bold">
+                                    BODY
+                                </h5>
+                                <div className="inputContainer">
+                                    <input type="text" className="modalInputBody rounded" />
+                                </div>
                             </div>
                         </div>
                     </div>
