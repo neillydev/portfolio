9ca7092161cfe8726c0c37f4b8252cf2bda5c45f
neillycubed <neillybusiness3@gmail.com>
2021-05-30 23:16:00 -0700

    add social media buttons with styling

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import ProjectGitIcon from '../Icons/ProjectGitIcon';
+import ProjectTwitterIcon from '../Icons/ProjectTwitterIcon';
 
 require('./Socials.css');
 
@@ -6,10 +8,19 @@ const Socials = () => {
     return (
         <div className='socialsContainer'>
             <ul className="socialsList">
-                
+                <li className="githubSocial">
+                    <button className="projSocial sideSocial">
+                            <ProjectGitIcon />
+                    </button>
+                </li>
+                <li className="githubSocial">
+                    <button className="projSocial sideSocial">
+                            <ProjectTwitterIcon />
+                    </button>
+                </li>
             </ul>
         </div>
     )
 }
 
-export default Socials
+export default Socials;
