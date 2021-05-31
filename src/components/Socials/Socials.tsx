b9f352327743492ecc71217dfb90666b6a4a1c23
neillycubed <neillybusiness3@gmail.com>
2021-05-30 22:59:27 -0700

    create Socials component for social media links

---
+++ src/components/Socials/Socials.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+require('./Socials.css');
+
+const Socials = () => {
+    return (
+        <div className='socialsContainer'>
+            <ul className="socialsList">
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Socials
