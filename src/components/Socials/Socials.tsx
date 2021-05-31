b280d88616e8d7e6bd2d29c681042a8baaf8439a
neillycubed <neillybusiness3@gmail.com>
2021-05-30 23:21:00 -0700

    add onClick func for github and twitter socials

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -4,17 +4,22 @@ import ProjectTwitterIcon from '../Icons/ProjectTwitterIcon';
 
 require('./Socials.css');
 
+const handleLinkBtn = (extLink: string) => {
+    window.open(extLink, '_blank');
+}
+
+
 const Socials = () => {
     return (
         <div className='socialsContainer'>
             <ul className="socialsList">
                 <li className="githubSocial">
-                    <button className="projSocial sideSocial">
+                    <button className="projSocial sideSocial" onClick={() => handleLinkBtn('https://github.com/neillydev')}>
                             <ProjectGitIcon />
                     </button>
                 </li>
                 <li className="githubSocial">
-                    <button className="projSocial sideSocial">
+                    <button className="projSocial sideSocial" onClick={() => handleLinkBtn('https://twitter.com/neillydev')}>
                             <ProjectTwitterIcon />
                     </button>
                 </li>
