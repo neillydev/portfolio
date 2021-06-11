d7636b85e9c52b0852174fc4d1afa7ce6cb867e3
neillycubed <neillybusiness3@gmail.com>
2021-06-10 23:42:59 -0700

    add media queries for NavBar responsiveness

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -26,29 +26,29 @@ const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) =>
         font-sans font-bold'
             role='navigation'>
             <div className="flex items-center w-screen">
-                <Link to='/' className='pl-32 text-lg'>
+                <Link to='/' className='xl:pl-32 lg:pl-28 md:pl-12 pl-8 xl:text-lg lg:text-lg md:text-lg sm:text-sm text-sm'>
                     &lt; Vernon Neilly /&gt;
                 </Link>
-                <div className="px-4 cursor-pointer md:hidden">
+                <div className="flex justify-end w-screen pr-8 cursor-pointer md:hidden">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </div>
             </div>
             <div className="flex flex-end">
-                <div className='items-center pr-16 xl:flex lg:flex md:flex sm:hidden hidden'>
-                    <button className="navBtn m-8 flex justify-content items-center" onClick={() => handleAbtBtn()}>
+                <div className='items-center 2xl:pr-16 xl:pr-16 lg:pr-4 md:pr-4 sm:pr-4 xl:flex lg:flex md:flex sm:hidden hidden'>
+                    <button className="navBtn xl:m-8 m-4 flex justify-content items-center" onClick={() => handleAbtBtn()}>
                         <p className="font-sans font-bold text-md">About</p>
                         <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
                     </button>
-                    <button className="navBtn m-8 flex items-center justify-content" onClick={() => handleProjectBtn()}>
+                    <button className="navBtn xl:m-8 m-4 flex items-center justify-content" onClick={() => handleProjectBtn()}>
                         <p className="font-sans font-bold text-md">Projects</p>
                         <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
                     </button>
-                    <button className="navBtn m-8 flex justify-content items-center" onClick={() => handleContactBtn()}>
+                    <button className="navBtn xl:m-8 m-4 flex justify-content items-center" onClick={() => handleContactBtn()}>
                         <p className="font-sans font-bold text-md">Contact</p>
                         <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
                     </button>
                 </div>
-                <div className="pr-12 items-end xl:flex lg:flex md:flex sm:hidden hidden">
+                <div className="xl:pr-12 lg:pr-4 md:pr-4 sm:pr-4 pr-4 items-end xl:flex lg:flex md:flex sm:hidden hidden">
                     <button className="navBtn m-8 flex justify-content items-center" onClick={() => handleTwitterBtn()}>
                         <TwitterIcon />
                         <p className="font-sans font-semibold text-sm ml-4">Twitter</p>
