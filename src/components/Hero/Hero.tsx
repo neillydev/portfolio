f53bf6e0d29274d9f03ffbe41af64a39d4165286
neillycubed <neillybusiness3@gmail.com>
2021-06-18 02:09:07 -0700

    refactor responsive design for Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,24 +9,24 @@ type HeroProps = {
 }
 const Hero = ({ handleProjectBtn, handleModalActive }: HeroProps) => {
     return (
-        <div className='heroContainer flex justify-center items-center'>
-            <div className="heroTitleContainer bg-white flex flex-col xl:pl-52 xl:pt-52 lg:pt-52 md:pt-52 pt-16">
+        <div className='heroContainer flex sm:justify-center items-center'>
+            <div className="heroTitleContainer bg-white flex flex-col xl:pl-52 xl:pt-52 lg:pl-32 lg:pt-32 md:pt-24 pt-16">
                 <div className="flex flex-col md:mb-2 mb-2">
                     <span className='shadow-lg bg-green-400 lg:w-52 md:w-44 w-36 lg:h-8 md:h-6 h-5 rounded-sm flex items-center justify-center'>
                         <h1 className='font-mono font-semibold lg:text-base md:text-sm text-xs'>Full Stack Developer</h1>
                     </span>
                 </div>
-                <h1 className='heroTitle lg:text-9xl md:text-7xl sm:text-5xl 
+                <h1 className='heroTitle xl:text-9xl md:text-8xl sm:text-5xl 
             text-5xl font-black'>
                 VERNON
                 <br />
                 NEILLY
             </h1>
-                <div className='flex flex-col lg:mt-10 md:mt-6 sm:mt-6'>
+                <div className='heroFlare flex-col lg:mt-10 md:mt-6 sm:mt-6'>
                     <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1 md:h-1 sm:h-1 rounded-md' />
                     <span className='bg-green-400 lg:w-72 md:w-48 sm:w-28 lg:h-1 md:h-1 sm:h-1 rounded-md lg:mt-10 md:mt-6 sm:mt-6 lg:ml-24 md:ml-16 sm:ml-16' />
                 </div>
-                <div className="heroBtnContainer pt-16 xl:flex lg:flex md:flex sm:flex hidden">
+                <div className="heroBtnContainer pt-16">
                     <button className="heroBtn border-2 rounded-lg font-bold w-44" onClick={() => handleProjectBtn()}>
                         Projects
                     </button>
@@ -35,7 +35,7 @@ const Hero = ({ handleProjectBtn, handleModalActive }: HeroProps) => {
                     </button>
                 </div>
             </div>
-            <div className="codeContainer flex flex-col rounded absolute shadow-xl border-2 self-center">
+            <div className="codeContainer flex flex-col rounded shadow-xl border-2 self-center">
                 <div className="codeHeader flex">
                     <div className="mockControls flex">
                         <span className="mockWindowControl bg-red-500 rounded-xl" />
