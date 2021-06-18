bbcfafd15f9e5c7dc50d017b5b06a69709589102
neillycubed <neillybusiness3@gmail.com>
2021-06-18 02:08:53 -0700

    refactor responsive design for About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,14 +9,14 @@ type AbtProps = {
 const About = ({ refObj }: AbtProps) => {
     return (
         <div ref={refObj} className="aboutContainer">
-            <div className="flex flex-col xl:pl-72 xl:pt-12">
+            <div className="aboutTitleContainer flex flex-col xl:pl-72 xl:pt-12 lg:pl-40 md:pl-40 sm:pl-20 lg:pr-32 md:pr-32 sm:pr-32 md:whitespace-nowrap sm:whitespace-nowrap">
                 <div className="aboutHeader flex">
-                    <h1 className='lg:text-6xl md:text-5xl sm:text-4xl 
+                    <h1 className='xl:text-6xl lg:text-5xl md:text-4xl sm:text-3xl 
             text-3xl font-black'>About Me</h1>
-                    <span className='aboutHeaderDash bg-green-200 lg:ml-8 ml-4 rounded-xl mt-10' />
+                    <span className='aboutHeaderDash bg-green-200 lg:ml-8 md:ml-4 sm:ml-4 rounded-xl xl:mt-10 lg:mt-8 md:mt-7 sm:mt-5' />
                 </div>
             </div>
-            <div className="aboutBody flex xl:justify-center lg:justify-center md:justify-center xl:p-32 lg:p-32 md:p-32 sm:p-32 p-4">
+            <div className="aboutBody flex xl:justify-center lg:justify-center md:justify-center xl:p-32 lg:p-32 md:p-24 sm:p-20 p-4">
                 <div className="aboutText font-semibold">
                     <p>
                         Hello! My name is Vernon and I build and design responsive and sleek applications for the world to enjoy! I started this journey as a developer when I was 10 years old, with a dream to create my own website!
