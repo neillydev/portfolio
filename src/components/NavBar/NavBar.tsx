c976f6a4c7ef1c4c5404014cf047df818f73f45b
neillycubed <neillybusiness3@gmail.com>
2021-06-19 02:18:09 -0700

    refactor hamburger menu responsiveness

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -29,12 +29,12 @@ const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) =>
                 <Link to='/' className='xl:pl-32 lg:pl-28 md:pl-12 pl-8 xl:text-lg lg:text-lg md:text-lg sm:text-sm text-xs whitespace-nowrap'>
                     &lt; Vernon Neilly /&gt;
                 </Link>
-                <div className="flex justify-end w-screen pr-8 cursor-pointer md:hidden">
+                <div className="flex justify-end w-screen pr-8 cursor-pointer lg:hidden">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </div>
             </div>
             <div className="flex flex-end">
-                <div className='items-center 2xl:pr-16 xl:pr-16 lg:pr-4 md:pr-4 sm:pr-4 xl:flex lg:flex md:flex sm:hidden hidden'>
+                <div className='items-center 2xl:pr-16 xl:pr-16 lg:pr-4 md:pr-4 sm:pr-4 xl:flex lg:flex md:hidden sm:hidden hidden'>
                     <button className="navBtn xl:m-8 m-4 flex justify-content items-center" onClick={() => handleAbtBtn()}>
                         <p className="font-sans font-bold text-md">About</p>
                         <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
@@ -48,7 +48,7 @@ const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) =>
                         <span className='btnHov absolute rounded h-1 bg-green-500 mt-12' />
                     </button>
                 </div>
-                <div className="xl:pr-12 lg:pr-4 md:pr-4 sm:pr-4 pr-4 items-end xl:flex lg:flex md:flex sm:hidden hidden">
+                <div className="xl:pr-12 lg:pr-4 md:pr-4 sm:pr-4 pr-4 items-end xl:flex lg:flex md:hidden sm:hidden hidden">
                     <button className="navBtn m-8 flex justify-content items-center" onClick={() => handleTwitterBtn()}>
                         <TwitterIcon />
                         <p className="font-sans font-semibold text-sm ml-4">Twitter</p>
