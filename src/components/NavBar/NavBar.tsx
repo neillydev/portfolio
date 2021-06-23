ced22495ddaeb688ef37cd419864c114cf4f7bc3
neillycubed <neillybusiness3@gmail.com>
2021-06-23 11:01:50 -0700

    add Hamburger component to NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,12 @@
 import React, {useRef, useLayoutEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import Hamburger from '../Hamburger/Hamburger';
+
 import TwitterIcon from '../Icons/TwitterIcon';
 import GitIcon from '../Icons/GitIcon';
 
-const styles = require('./NavBar.css');
+require('./NavBar.css');
 
 type NavProps = {
     handleProjectBtn: () => void,
@@ -25,13 +27,14 @@ const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) =>
         <nav className='flex items-center h-16 bg-white text-black relative shadow-sm
         font-sans font-bold'
             role='navigation'>
-            <div className="flex items-center w-screen">
+            <div className="flex items-center w-screen justify-between">
                 <Link to='/' className='xl:pl-32 lg:pl-28 md:pl-12 pl-8 xl:text-lg lg:text-lg md:text-lg sm:text-sm text-xs whitespace-nowrap'>
                     &lt; Vernon Neilly /&gt;
                 </Link>
-                <div className="flex justify-end w-screen pr-8 cursor-pointer lg:hidden">
+                {/* <div className="flex justify-end w-screen pr-8 cursor-pointer lg:hidden">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
-                </div>
+                </div> */}
+                <Hamburger />
             </div>
             <div className="flex flex-end">
                 <div className='items-center 2xl:pr-16 xl:pr-16 lg:pr-4 md:pr-4 sm:pr-4 xl:flex lg:flex md:hidden sm:hidden hidden'>
