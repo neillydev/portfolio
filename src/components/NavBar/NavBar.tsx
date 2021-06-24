3a5c1ddeedca9d4f1c4f3a6531ff9015f95044d5
neillycubed <neillybusiness3@gmail.com>
2021-06-24 13:38:48 -0700

    refactor NavBar props for Hamburger component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -12,6 +12,8 @@ type NavProps = {
     handleProjectBtn: () => void,
     handleAbtBtn: () => void,
     handleContactBtn: () => void,
+    handleHamburgerHelped: () => void,
+    hamburgerHelper: boolean
 }
 
 const handleGitBtn = () => {
@@ -22,7 +24,7 @@ const handleTwitterBtn = () => {
     window.open('https://twitter.com/neillydev', '_blank');
 }
 
-const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) => {
+const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn, handleHamburgerHelped, hamburgerHelper}: NavProps) => {
     return (
         <nav className='flex items-center h-16 bg-white text-black relative shadow-sm
         font-sans font-bold'
@@ -34,7 +36,7 @@ const NavBar = ({handleProjectBtn, handleAbtBtn, handleContactBtn}: NavProps) =>
                 {/* <div className="flex justify-end w-screen pr-8 cursor-pointer lg:hidden">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
                 </div> */}
-                <Hamburger />
+                <Hamburger handleHamburgerHelped={handleHamburgerHelped} hamburgerHelper={hamburgerHelper} />
             </div>
             <div className="flex flex-end">
                 <div className='items-center 2xl:pr-16 xl:pr-16 lg:pr-4 md:pr-4 sm:pr-4 xl:flex lg:flex md:hidden sm:hidden hidden'>
