f41c642965159737f1fd13cd9d2790ec1ac1bced
neillycubed <neillybusiness3@gmail.com>
2021-06-24 13:22:07 -0700

    refactor Hamburger menu with state

--- src/components/Hamburger/Hamburger.tsx
+++ src/components/Hamburger/Hamburger.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Exit from '../Icons/Exit';
 
 require('./Hamburger.css');
 
 const Hamburger = () => {
+    const [hamburgerHelper, setHamburgerHelper] = useState(false);
+
     return (
-        <div className="hamburgerContainer relative flex justify-end cursor-pointer">
-            <div className='hamburgerTop'/>
-            <div className='hamburgerMeat'/>
-            <div className='hamburgerBottom'/>
+        <div className={`hamburgerContainer relative flex justify-end cursor-pointer ${hamburgerHelper && 'hamburgerAnim'}`} onClick={() => setHamburgerHelper(!hamburgerHelper)}>
+            <div className="hamburgerLines">
+                {hamburgerHelper ?
+                    <Exit />
+                    :
+                    [
+                        <div className='hamburgerTop' />,
+                        <div className='hamburgerMeat' />,
+                        <div className='hamburgerBottom' />
+                    ]
+                }
+            </div>
         </div>
     )
 };
