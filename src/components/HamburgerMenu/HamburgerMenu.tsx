d472813ea95406443275c5bf1ef6a47523759c85
neillycubed <neillybusiness3@gmail.com>
2021-06-24 19:20:26 -0700

    create mobile Nav buttons for Hamburger Menu

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -4,8 +4,16 @@ require('./HamburgerMenu.css');
 
 const HamburgerMenu = () => {
     return (
-        <div className='hamburgerMenuContainer hamburgerOverlayAnimation'>
-            
+        <div className='hamburgerMenuContainer hamburgerOverlayAnimation flex flex-col items-center p-12'>
+            <button className="mobileNavBtn mobileAbout font-sans font-bold text-4xl p-4">
+                <p>About</p>
+            </button>
+            <button className="mobileNavBtn mobileProjects font-sans font-bold text-4xl p-4">
+                <p>Projects</p>
+            </button>
+            <button className="mobileNavBtn mobileContact font-sans font-bold text-4xl p-4">
+                <p>Contact</p>
+            </button>
         </div>
     )
 }
